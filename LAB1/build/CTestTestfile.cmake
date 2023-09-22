17cf4ba8b21c07c46d202f18ff7eba12775cf164
tng00 <tnguen00@gmail.com>
2023-09-22 09:14:40 +0300

    edit cmakelist

--- LAB1/build/CTestTestfile.cmake
+++ LAB1/build/CTestTestfile.cmake
@@ -5,5 +5,5 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(BASIC "C:/OOP/LAB1/build/test1.exe")
-set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "C:/OOP/LAB1/CMakeLists.txt;12;add_test;C:/OOP/LAB1/CMakeLists.txt;0;")
+set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "C:/OOP/LAB1/CMakeLists.txt;11;add_test;C:/OOP/LAB1/CMakeLists.txt;0;")
 subdirs("googletest")
